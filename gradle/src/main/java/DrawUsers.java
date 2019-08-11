cba9a04e1dc0bcc9c596820e27020377604a2d11
chammill <chammill@luc.edu>
2019-08-10 20:03:51 -0700

    Added simple drawing for users

---
+++ gradle/src/main/java/DrawUsers.java
@@ -0,0 +1,24 @@
+import POJO.User;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class DrawUsers {
+
+    // DrawUsers needs clone the create users list
+    // Shuffle that list
+    // and then match up in order
+    // will add functionality for self later
+
+    public static void shuffleList() {
+        ArrayList<User> shuffledUsers = (ArrayList<User>)CreateUsers.userList.clone();
+        Collections.shuffle(shuffledUsers);
+
+        for (int i = 0; i<shuffledUsers.size(); i++) {
+            CreateUsers.userList.get(i).setBuyingFor(shuffledUsers.get(i));
+        }
+
+    }
+
+
+}
--- gradle/src/main/java/MainClass.java
+++ gradle/src/main/java/MainClass.java
@@ -2,6 +2,13 @@ public class MainClass {
 
     public static void main(String [] args) {
         CreateUsers.start();
+        DrawUsers.shuffleList();
+
+        for (int i=0; i<CreateUsers.userList.size(); i++) {
+            System.out.println("Name: " + CreateUsers.userList.get(i).getName());
+            System.out.println("Buying For: " + CreateUsers.userList.get(i).getBuyingFor().getName());
+        }
+
     }
 
 }
